248cba7c208286b4c7a70d3210233bf87dafc133
Hai Tran Tuan <59082486+tuanhai512@users.noreply.github.com>
2021-11-14 15:31:05 +0700

    update manager

---
+++ THDShop/Manager/Controllers/CategoryController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Manager.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        QLWBLTEntities _db = new QLWBLTEntities();
+        // GET: NhanVien/LoaiMon
+        public ActionResult Index()
+        {
+            if (Session["IDQL"] == null)
+            {
+                return RedirectToAction("Index", "LoginQuanLy");
+            }
+            return View(_db.Categories.ToList());
+        }
+
+        // GET: NhanVien/LoaiMon/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(_db.Categories.Where(s => s.ID == id).FirstOrDefault());
+        }
+
+        // GET: NhanVien/LoaiMon/Create
+        public ActionResult Create()
+        {
+            Category loai = new Category();
+            return View(loai);
+        }
+
+        // POST: NhanVien/LoaiMon/Create
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                // TODO: Add insert logic here
+                {
+                    _db.Categories.Add(category);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(category);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NhanVien/LoaiMon/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(_db.Categories.Where(s => s.ID == id).FirstOrDefault());
+        }
+
+        // POST: NhanVien/LoaiMon/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Category loai, int id)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                if (ModelState.IsValid)
+                {
+                    _db.Entry(loai).State = EntityState.Modified;
+                    _db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(loai);
+            }
+            catch
+            {
+                return Content("Data đang được sử dụng bởi một bảng khác");
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(_db.Categories.Where(s => s.ID == id).FirstOrDefault());
+        }
+
+        // POST: NhanVien/DonViTinhMon/Delete/5
+        [HttpPost]
+        public ActionResult Delete(Category category, int id)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                category = _db.Categories.Where(s => s.ID == id).FirstOrDefault();
+                _db.Categories.Remove(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return Content("Dữ liệu này đang được sử dụng bởi một bảng khác");
+            }
+        }
+
+        public PartialViewResult LoaiPartial()
+        {
+            var loaiList = _db.Categories.ToList();
+            return PartialView(loaiList);
+        }
+    }
+}
---
+++ THDShop/Manager/Controllers/RoleController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Manager.Controllers
+{
+    public class RoleController : Controller
+    {
+        // GET: Role
+        QLWBLTEntities database = new QLWBLTEntities();
+        public ActionResult Index()
+        {
+            if (Session["IDQL"] == null)
+            {
+                return RedirectToAction("Index", "LoginQuanLy");
+            }
+            return View(database.Roles.ToList());
+        }
+
+        // GET: QuanLy/KhachHang/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(database.Roles.Where(s => s.ID == id).FirstOrDefault());
+        }
+
+        // GET: QuanLy/KhachHang/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: QuanLy/KhachHang/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: QuanLy/KhachHang/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(database.Roles.Where(s => s.ID == id).FirstOrDefault());
+        }
+
+        // POST: QuanLy/Account/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Role role)
+        {
+            var detail = database.Roles.Where(s => s.ID == role.ID);
+
+            if (detail == null)
+            {
+                return HttpNotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                database.Entry(role).State = EntityState.Modified;
+                database.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // GET: QuanLy/KhachHang/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: QuanLy/KhachHang/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        } 
+    }
+}
